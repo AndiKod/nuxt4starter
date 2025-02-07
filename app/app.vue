fcef97a8a2adb62c54e4edbd3cb54d27808b42b9
AndiKod <andikod@proton.me>
2025-02-07 14:53:49 +0100

    ...almost.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <h1>Welcome to Nuxt4, well, almost.</h1> 
+    <h1>Welcome to <a href="https://nuxt.com/docs/getting-started/upgrade#opting-in-to-nuxt-4">Nuxt4</a>! Well, ...almost.</h1> 
+    <p>You can create an `app/pages` folder, and move `app.vue` there, renamed as `index.vue`.</p> 
+    <a href="https://github.com/AndiKod/nuxt4starter">github.com/AndiKod/nuxt4starter</a>
   </div>
 </template>
+
+<style>
+  body {
+    padding: 1rem;
+    text-align: center;
+    font-family: sans-serif;
+    background-color: #0c1723;
+    color: #e3e7ed;
+  }
+  a {
+    color: #50e57a;
+  }
+</style>
